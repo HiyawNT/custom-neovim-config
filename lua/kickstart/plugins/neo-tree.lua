b8c3557537860b4d47b293f3f99a959f3c53dbd3
HiyawNT <hiyaw.temitim@gmail.com>
2025-11-23 20:24:11 +0300

    Changed the NeoTree(file explorer) toggle and keybindings from `\` to `<leader>e` and other convinent keybindings found in the `kickstarter/plugins/neo-tree.lua`

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -1,23 +1,36 @@
 -- Neo-tree is a Neovim plugin to browse the file system
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
-
+-- lua/plugins/neo-tree.lua  (or wherever you keep it)
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'nvim-tree/nvim-web-devicons',
     'MunifTanjim/nui.nvim',
   },
   lazy = false,
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>e', '<cmd>Neotree toggle<CR>', desc = 'NeoTree: Toggle file explorer' },
+    { '<leader>o', '<cmd>Neotree reveal<CR>', desc = 'NeoTree: Reveal current file' },
   },
   opts = {
+    close_if_last_window = true, -- close Neo-tree if it's the last window
+    popup_border_style = 'rounded',
+    enable_git_status = true,
+    enable_diagnostics = true,
     filesystem = {
+      follow_current_file = { enabled = true }, -- auto-reveal current file
+      hijack_netrw_behavior = 'open_current',
+      use_libuv_file_watcher = true,
       window = {
         mappings = {
-          ['\\'] = 'close_window',
+          ['<leader>e'] = 'close_window',
+          ['h'] = 'toggle_hidden',
+          ['H'] = 'toggle_node',
+          ['l'] = 'open',
+          ['<CR>'] = 'open',
+          ['o'] = 'open',
         },
       },
     },
