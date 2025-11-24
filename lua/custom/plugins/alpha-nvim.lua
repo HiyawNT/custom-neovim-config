48b9f48adc42357b9a7f09de7eb993dc6dbfc525
HiyawNT <hiyaw.temitim@gmail.com>
2025-11-24 20:39:14 +0300

    Added custom Startup Dashboard also commented out `avante` since I want it to be optional

---
+++ lua/custom/plugins/alpha-nvim.lua
@@ -0,0 +1,47 @@
+return {
+  'goolord/alpha-nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    local alpha = require 'alpha'
+    local dashboard = require 'alpha.themes.dashboard'
+    -- or change to this
+    -- dashboard.section.header.val = {
+    --   [[  ███╗   ██╗ ███████╗  ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗ ]],
+    --   [[  ████╗  ██║ ██╔════╝ ██╔═══██╗ ██║   ██║ ██║ ████╗ ████║ ]],
+    --   [[  ██╔██╗ ██║ █████╗   ██║   ██║ ██║   ██║ ██║ ██╔████╔██║ ]],
+    --   [[  ██║╚██╗██║ ██╔══╝   ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║ ]],
+    --   [[  ██║ ╚████║ ███████╗ ╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║ ]],
+    --   [[  ╚═╝  ╚═══╝ ╚══════╝  ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝ ]],
+    -- }
+
+    dashboard.section.header.val = {
+      [[  ▀███▄   ▀███▀███▀▀▀███  ▄▄█▀▀██▄ ▀████▀   ▀███▀████▀████▄     ▄███▀ ]],
+      [[    ███▄    █   ██    ▀█▄██▀    ▀██▄ ▀██     ▄█   ██   ████    ████   ]],
+      [[    █ ███   █   ██   █  ██▀      ▀██  ██▄   ▄█    ██   █ ██   ▄█ ██   ]],
+      [[    █  ▀██▄ █   ██████  ██        ██   ██▄  █▀    ██   █  ██  █▀ ██   ]],
+      [[    █   ▀██▄█   ██   █  ▄█▄      ▄██   ▀██ █▀     ██   █  ██▄█▀  ██   ]],
+      [[    █     ███   ██     ▄███▄    ▄██▀    ▄██▄      ██   █  ▀██▀   ██   ]],
+      [[  ▄███▄    ██ ▄██████████ ▀▀████▀▀       ██     ▄████▄███▄ ▀▀  ▄████▄ ]],
+    }
+
+    dashboard.section.buttons.val = {
+      dashboard.button('f', '  Find File', ':Telescope find_files <CR>'),
+      dashboard.button('e', '  New File', ':ene <BAR> startinsert <CR>'),
+      dashboard.button('r', '  Recent', ':Telescope oldfiles <CR>'),
+      dashboard.button('s', '  Settings', ':e $MYVIMRC <CR>'),
+      dashboard.button('q', '  Quit', ':qa<CR>'),
+    }
+    dashboard.section.footer.val = {
+      '',
+      '',
+      '',
+      '',
+      '                                                             by hiyaw',
+    }
+    dashboard.section.footer.opts = { hl = 'Comment', position = 'center' }
+
+    -- Footer will now show up perfectly aligned to the right
+    alpha.setup(dashboard.opts)
+    alpha.setup(dashboard.opts)
+  end,
+}
